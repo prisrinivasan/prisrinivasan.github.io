6b9d56cd94474be0d2283d55e17f6034e7a77486
prisrinivasan <priyanka.srinivasan2013@gmail.com>
2020-06-27 15:31:43 -0400

    ....

---
+++ covid_shiny/app.R
@@ -0,0 +1,7 @@
+rsconnect::setAccountInfo(name = 'prisrinivasan',
+                          token = 'F736CD4CDF0F6995DA2DC4E100A311BE',
+                          secret = 'e4xCam6qJm7OMBZWKETw+27eSiDIxoGbTaeQeRdR')
+
+library(rsconnect)
+rsconnect::deployApp('./covidshiny.Rmd')
+
